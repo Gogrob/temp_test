ec836a01525ca90f8aa82407cf6620db6087d2e0
Rob Whybrew <gogrob@gmail.com>
2018-02-17 13:07:03 -0600

    first commit

---
+++ main.c
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.c
+ * Author: rob
+ *
+ * Created on December 10, 2017, 2:29 PM
+ */
+// test
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    // initialize the device
+    //SYSTEM_Initialize();
+    
+    while (1)
+    {
+        // Add your application code
+        printf("Rob is great \n");
+        
+    }
+
+    printf("Rob is great\n");
+    
+    return (EXIT_SUCCESS);
+}
+
---
+++ newmain.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   newmain.c
+ * Author: rob
+ *
+ * Created on December 10, 2017, 2:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Rob is great \n");
+           
+    return (EXIT_SUCCESS);
+}
+
